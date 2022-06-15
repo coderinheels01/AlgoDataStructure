af79dc72c6cb37a4c18441b4daaab784e84ae380
coderinheels01 <emily.aung2017@gmail.com>
2022-06-14 23:56:32 -0400

    Trie test

--- src/algorithm/TrieMain.java
+++ src/algorithm/TrieMain.java
@@ -3,18 +3,6 @@ package algorithm;
 public class TrieMain {
 
     public static void main(String ...args) {
-//        Trie trie = new Trie();
-//        trie.insert("apple");
-//        System.out.println("is the word apple exist? " + trie.search("apple"));
-//        System.out.println("is the prefix app exist? " + trie.startsWith("app"));
-//        System.out.println("is the word app exist? " + trie.search("app"));
-//        trie.insert("app");
-//        System.out.println("is the word app exist? " + trie.search("app"));
-//
-//        Trie trie2 = new Trie();
-//        trie.insert("hotdog");
-//        System.out.println("is the prefix dog exist? " + trie2.startsWith("dog"));
-
 
         Trie trie = new Trie();
         trie.insert("app");
@@ -23,48 +11,48 @@ public class TrieMain {
         trie.insert("add");
         trie.insert("jam");
         trie.insert("rental");
-//        String word  ="apple";
-//        System.out.println("is the word " + word +" exist? " + trie.search(word));
-//        word  ="apps";
-//        System.out.println("is the word " + word +" exist? " + trie.search(word));
-//        word  ="app";
-//        System.out.println("is the word " + word +" exist? " + trie.search(word));
-//        word  ="ad";
-//        System.out.println("is the word " + word +" exist? " + trie.search(word));
-//        word  ="applepie";
-//        System.out.println("is the word " + word +" exist? " + trie.search(word));
-//        word  ="rest";
-//        System.out.println("is the word " + word +" exist? " + trie.search(word));
-//
-//        word  ="jan";
-//        System.out.println("is the word " + word +" exist? " + trie.search(word));
-//
-//        word  ="rent";
-//        System.out.println("is the word " + word +" exist? " + trie.search(word));
-//
-//        word  ="beer";
-//        System.out.println("is the word " + word +" exist? " + trie.search(word));
-//
-//        word  ="jam";
-//        System.out.println("is the word " + word +" exist? " + trie.search(word));
+        String word  ="apple";
+        System.out.println("is the word " + word +" exist? " + trie.search(word));
+        word  ="apps";
+        System.out.println("is the word " + word +" exist? " + trie.search(word));
+        word  ="app";
+        System.out.println("is the word " + word +" exist? " + trie.search(word));
+        word  ="ad";
+        System.out.println("is the word " + word +" exist? " + trie.search(word));
+        word  ="applepie";
+        System.out.println("is the word " + word +" exist? " + trie.search(word));
+        word  ="rest";
+        System.out.println("is the word " + word +" exist? " + trie.search(word));
+
+        word  ="jan";
+        System.out.println("is the word " + word +" exist? " + trie.search(word));
+
+        word  ="rent";
+        System.out.println("is the word " + word +" exist? " + trie.search(word));
+
+        word  ="beer";
+        System.out.println("is the word " + word +" exist? " + trie.search(word));
+
+        word  ="jam";
+        System.out.println("is the word " + word +" exist? " + trie.search(word));
 
         String prefix = "apps";
-        //System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
+        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
         prefix = "app";
         System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
-//        prefix = "ad";
-//        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
-//        prefix = "applepie";
-//        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
-//        prefix = "rest";
-//        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
-//        prefix = "jan";
-//        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
-//        prefix = "rent";
-//        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
-//        prefix = "beer";
-//        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
-//        prefix = "jam";
-//        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
+        prefix = "ad";
+        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
+        prefix = "applepie";
+        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
+        prefix = "rest";
+        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
+        prefix = "jan";
+        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
+        prefix = "rent";
+        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
+        prefix = "beer";
+        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
+        prefix = "jam";
+        System.out.println("is the prefix " + prefix +" exist? " + trie.startsWith(prefix));
     }
 }
