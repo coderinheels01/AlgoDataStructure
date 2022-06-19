1b76056db10f6c6e5c4ca9038e3b19c2b75c80e9
coderinheels01 <emily.aung2017@gmail.com>
2022-06-19 18:03:19 -0400

    Leetcode 143. Reorder List. update the merge to better one

--- src/algorithm/ReorderList.java
+++ src/algorithm/ReorderList.java
@@ -6,11 +6,15 @@ import algorithm.util.PrintUtil;
 public class ReorderList {
 
     /*
-     * 1. find the middle of the node using slow and fast pointers.
+     * 1. find the middle of the node using slow and fast pointers. Keep a pointer called slowTail to keep the previous pointer of slow. so we can set it to null
+     *    and break the link list in half.
      * 2. reverse half the list once the slow pointer is pointing to the middle of the list.
-     * 3. create a new head and set values of half of original list and reversed list alternatively
-     *    by checking if current node count is even or odd. if it's even set the node with non-reversed value.
-     *    otherwise set it with value from reversed list.
+     * 3. merge the two lists
+     *     a) get the pointer of first.next and second.next.
+     *     b) set the first.next to second. ( 1 -> 5)
+     *     c) if firstNext is null break out of the loop. because we have already merged the list successfully.
+     *     d) set second.next to first.next ( 1 -> 5 -> 2)
+     *     e) move both pointers to next, first = firstNext; second = secondNext;
      *
      * Time Complexity: O(N)
      * Space complexity :O(1)
@@ -21,12 +25,13 @@ public class ReorderList {
     public static void reorderListOptimized(Node head) {
         Node slow = head;
         Node fast = head;
-
+        Node slowTail = null;
         while(fast != null && fast.getNext() != null){
+            slowTail= slow;
             slow = slow.getNext();
             fast = fast.getNext().getNext();
         }
-
+        slowTail.setNext(null);
         Node prev = null;
         Node current = slow;
 
@@ -37,22 +42,50 @@ public class ReorderList {
            current = next;
         }
 
-        Node tempHead = new Node();
-        Node newHead = tempHead;
-        int count = 0;
-        while(prev != null && head != null){
-            if(count%2 == 0){
-                tempHead.setNext(head);
-                head = head.getNext();
-            }
-            else{
-                tempHead.setNext(prev);
-                prev = prev.getNext();
-            }
-            count++;
-            tempHead = tempHead.getNext();
+        Node first = head;
+        Node second = prev;
+
+        while(first != null){
+            Node firstNext = first.getNext();
+            Node secondNext = second.getNext();
+
+            first.setNext(second);
+            if(firstNext == null)
+                break;
+
+            second.setNext(firstNext);
+
+            first= firstNext;
+            second = secondNext;
         }
-        PrintUtil.printLinkedList(newHead.getNext());
+
+        PrintUtil.printLinkedList(head);
+
+//        Node tempHead = new Node();
+//        Node newHead = tempHead;
+//        int count = 0;
+//        while(prev != null && head != null){
+//            if(count%2 == 0){
+//                tempHead.setNext(head);
+//                head = head.getNext();
+//            }
+//            else{
+//                tempHead.setNext(prev);
+//                prev = prev.getNext();
+//            }
+//            count++;
+//            tempHead = tempHead.getNext();
+//        }
+//
+//        while(prev != null){
+//            tempHead.setNext(prev);
+//            prev = prev.getNext();
+//            tempHead = tempHead.getNext();
+//        }
+
+
+//PrintUtil.printLinkedList(newHead.getNext());
+
     }
 
     public static void main (String... args) {
